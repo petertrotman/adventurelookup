f261dc6074fafc4b66c4a96f29ed6cda3dc30116
= <petertrotman@gmail.com>
2016-08-29 19:05:56 +0100

    Small changes for CD test.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 cd /root/adventurelookup
 git pull
 docker-compose -f docker-compose.prod.yml up --build -d
+docker-compose -f docker-compose.prod.yml run --rm -u root api python manage.py makemigrations
+docker-compose -f docker-compose.prod.yml run --rm -u root api python manage.py migrate
 docker-compose -f docker-compose.prod.yml restart nginx
