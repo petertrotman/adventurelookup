94fd4d5bb7d59b9122ce58aa516b0ed3aca04d2c
Dylan Evans <dylanlee.evans95@gmail.com>
2016-09-09 21:42:39 +0100

    Simplify Author model testing
    The only purpose of testing the models is to make sure that we can
    use the expected keyword arguments during object creation. For this
    purpose, full CRUD testing is not necessary. Testing object creation
    is enough.

--- server/adventures/tests.py
+++ server/adventures/tests.py
@@ -3,17 +3,7 @@ from .models import Author, Publisher, Edition, Setting, Adventure
 
 
 class AuthorTests(TestCase):
-    def setUp(self):
-        self.gygax = Author.objects.create(name='Gary Gygax')
-
     def test_create_author(self):
-        self.assertEqual(Author.objects.first(), self.gygax)
+        gygax = Author.objects.create(name='Gary Gygax')
+        self.assertEqual(Author.objects.first(), gygax)
         self.assertEqual(Author.objects.count(), 1)
-
-    def test_update_author(self):
-        Author.objects.filter(id=self.gygax.id).update(name='Joe Bloggs')
-        self.assertEqual(Author.objects.first().name, 'Joe Bloggs')
-
-    def test_delete_author(self):
-        Author.objects.filter(id=self.gygax.id).delete()
-        self.assertEqual(Author.objects.count(), 0)
