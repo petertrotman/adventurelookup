3fed1e16d6fa2a0d0c4b5a9f2b9ee1892a74fae3
Dylan Evans <dylanlee.evans95@gmail.com>
2016-09-09 21:47:21 +0100

    Add Publisher model creation test

--- server/adventures/tests.py
+++ server/adventures/tests.py
@@ -7,3 +7,10 @@ class AuthorTests(TestCase):
         gygax = Author.objects.create(name='Gary Gygax')
         self.assertEqual(Author.objects.first(), gygax)
         self.assertEqual(Author.objects.count(), 1)
+
+
+class PublisherTests(TestCase):
+    def test_create_author(self):
+        wotc = Publisher.objects.create(name='Wizards of the Coast')
+        self.assertEqual(Publisher.objects.first(), wotc)
+        self.assertEqual(Publisher.objects.count(), 1)
