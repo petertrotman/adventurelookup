2bbaf48ab7c8a94b53f1f191449a2f525dce62f3
Dylan Evans <dylanlee.evans95@gmail.com>
2016-09-09 15:41:55 +0100

    Add basic models
    The Adventure model is currently very minimal. It does
    not track things like the items, monsters or NPCs in
    an adventure.

--- server/adventures/models.py
+++ server/adventures/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Publisher(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Edition(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Adventure(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    title = models.TextField()
+    edition = models.ForeignKey(Edition)
+    authors = models.ManyToManyField(Author)
+    edition = models.ForeignKey(Publisher)
+    published = models.DateField()
+    min_level = models.IntegerField(help_text=('The minumum character level '
+                                               'for the adventure'))
+    max_level = models.IntegerField(help_text=('The maximum character level '
+                                               'for the adventure'))
+    min_characters = models.IntegerField(help_text=('The minumum number of '
+                                                    'charactersfor the '
+                                                    'adventure'))
+    max_characters = models.IntegerField(help_text=('The maximum number of '
+                                                    'charactersfor the '
+                                                    'adventure'))
