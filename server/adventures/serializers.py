68d2fa96ff9a45217203c6b9993bcffdc597a964
Dylan Evans <dylanlee.evans95@gmail.com>
2016-09-10 15:49:02 +0100

    Add Author model serializer

--- server/adventures/serializers.py
+++ server/adventures/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
-from .models import Adventure
+from .models import Author, Adventure
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
 
 
 class AdventureSerializer(serializers.ModelSerializer):
