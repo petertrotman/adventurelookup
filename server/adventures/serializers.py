8d5e17093e6f70b44e970f1d78130ce4d7decd2d
Dylan Evans <dylanlee.evans95@gmail.com>
2016-09-10 15:50:18 +0100

    Add Publisher serializer

--- server/adventures/serializers.py
+++ server/adventures/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
-from .models import Author, Adventure
+from .models import Author, Publisher, Adventure
 
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
+
+
+class PublisherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Publisher
 
 
 class AdventureSerializer(serializers.ModelSerializer):
